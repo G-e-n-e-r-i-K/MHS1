cb70e03b82835b60ea9ff0676290fe76499b67ec
above <kanishkad@gmail.com>
2019-10-12 11:56:38 +1100

    quick update- button toggle and more irchecks

--- practisemaybeidk.py
+++ practisemaybeidk.py
@@ -3,10 +3,13 @@ import ev3dev.ev3 as ev3
 import time
 import sys
 import Motor as motors
+from ev3dev.ev3 import Sound
 IR = ev3.Sensor("in1", driver_name = "ht-nxt-ir-seek-v2") 
 IR.mode = "DC-ALL"
 Compass = ev3.Sensor("in3", driver_name = "ht-nxt-compass")
 Compass.mode = "COMPASS"
+button = ev3.Button()
+Paused = False
 
 MA = ev3.LargeMotor("outA")
 MB = ev3.LargeMotor("outB")
@@ -33,15 +36,15 @@ init_cmps_value = cmps_init()
 curr180 = 150
 
 def cmps_correct(curr_heading, trim = 15):
-    print("cmps crct now")
+    #print("cmps crct now")
     if 90 < curr_heading < curr180:
-        print("spinning left")
+        #print("spinning left")
         motors.spinLeft(0.1)
     elif curr180 < curr_heading < 225:
-        print("spinning right")
+        #print("spinning right")
         motors.spinRight(0.1)
     else: 
-        print("in range")
+        #print("in range")
         pass
 
 def cmpscorr():
@@ -51,22 +54,23 @@ def cmpscorr():
         curr_heading = get_current_heading(init_cmps_value)
         cmps_correct(curr_heading)
 
-
 def main():
     IROut = IR.value(0)
     IRVal = IR.value(3)
     if 0 < IROut < 4:
-        print('Ball is on the left: 0<IROut<4')
+        #print('Ball is on the left: 0<IROut<4')
         motors.spinLeft()
     elif 4 < IROut < 6:
-        print('Ball is on the in front: 4<IROut<6')
+        #print('Ball is on the in front: 4<IROut<6')
         motors.forward(1000,0.5)                          
         motors.backwards(1000,0.5)                                    
     elif 9 >= IROut > 6:
-        print('Ball is on the right: 9>=IROut>6')
+        #print('Ball is on the right: 9>=IROut>6')
         motors.spinRight()
     elif IROut == 0:
-        print("no ball seen")
+        #print("no ball seen")
+        IROut = IR.value(0)
+        if IROut != 0: pass
         motors.spinRight(0.5)
         IROut = IR.value(0)
         if IROut != 0: pass
@@ -76,9 +80,23 @@ def main():
         if IROut != 0: pass
         time.sleep(1)
         motors.spinRight(0.5)
+        IROut = IR.value(0)
+        if IROut != 0: pass
         time.sleep(1)
-
     cmpscorr()
 
+loop_on = False
+
 while True:
-    main()
+    if loop_on:
+        main()
+    if button.enter:
+        Sound.tone(1500,100).wait()
+        if loop_on:
+            loop_on = False
+        else:
+            loop_on = True
+        while button.enter:
+            pass
+    
+    
